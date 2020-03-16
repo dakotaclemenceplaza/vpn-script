18269f6ae3254a9737a19315dd6ee1541ce3411e
A D <dakotaclemenceplaza@protonmail.com>
2020-03-16 04:19:39 +0400

    Add better names

--- vpn.hs
+++ vpn.hs
@@ -28,34 +28,36 @@ main = do
   result <- runExceptT $ run args
   case result of
     Right vpn -> case vpn of
-      On country -> putStrLn $ "Vpn is started. Location: " <> country
+      On server -> putStrLn $ "Vpn is started. Location: " <> server
       Off -> putStrLn "Vpn is stopped"
     Left e -> putStrLn e
 
 run :: [String] -> VpnToggle
-run [] = start defaultVpn
-run ["test"] = lift $ getLocation >>= \(Country c) -> notifyConnected (Country c) >> return (On (show c))
+run [] = start defaultServer
+-- maybe getting location shouldn't be in the main monad? It shouldn't always return a vpn
+-- or it needs to check if vpn is running before returning On or Off
+run ["location"] = lift $ getLocation >>= \(Country c) -> notifyConnected (Country c) >> return (On (show c))
 run ["next"] = next
 run ["stop"] = stop
-run [country] = if country `elem` vpns
-                 then start country
-                 else throwError "No such vpn"
+run [server] = if server `elem` servers
+                 then start server
+                 else throwError "No such vpn server"
 run _ = throwError "Too many arguments"
 
 start :: String -> VpnToggle  
-start country = do
+start server = do
   current <- currentVpn
   case current of
     On c -> throwError $ "Vpn is already running: " <> c
     Off -> do
-      stat <- status (On country)
+      stat <- status (On server)
       case stat of
         Off -> lift $ do
-          void $ runProcess $ shell $ "sudo /etc/init.d/openvpn." <> country <> " start"
+          void $ runProcess $ shell $ "sudo /etc/init.d/openvpn." <> server <> " start"
           getLocation >>= notifyConnected -- needs some time before asking location
           createDirectoryIfMissing False "/tmp/vpn" -- add checking status of just started vpn?
-          writeFile "/tmp/vpn/current" country
-          return (On country)
+          writeFile "/tmp/vpn/current" server
+          return (On server)
         On _ -> throwError "Error: running vpn is not in tmp file"
 
 next :: VpnToggle
@@ -70,10 +72,10 @@ stop :: VpnToggle
 stop = do
   current <- currentVpn
   case current of
-    On country -> lift $ do
-      runProcess_ $ shell $ "sudo /etc/init.d/openvpn." <> country <> " stop" 
+    On server -> lift $ do
+      runProcess_ $ shell $ "sudo /etc/init.d/openvpn." <> server <> " stop" 
       removeFile "/tmp/vpn/current" -- add checking status of just stopped?
-      return (On country)
+      return (On server)
     Off -> throwError "Vpn is not running"
 
 currentVpn :: VpnToggle
@@ -82,26 +84,26 @@ currentVpn = let exceptionHandler :: IOException -> IO Vpn
              in do
   current <- lift (fmap On (readFile "/tmp/vpn/current") `catch` exceptionHandler) >>= status
   case current of
-    On country -> return $ On country
+    On server -> return $ On server
     Off -> throwError "Error: current vpn in tmp file is not running"
 
 status :: Vpn -> VpnToggle
-status (On country) = do
-  (_, out, _) <- lift $ readProcess $ shell $ "sudo /etc/init.d/openvpn." <> country <> " status"
+status (On server) = do
+  (_, out, _) <- lift $ readProcess $ shell $ "sudo /etc/init.d/openvpn." <> server <> " status"
   case out of
     " * status: stopped\n" -> return Off
-    " * status: started\n" -> return $ On country
+    " * status: started\n" -> return $ On server
     _ -> throwError "Error: something unexpected in status checking"
 status Off = return Off
 
-vpns :: [String]
-vpns = ["nl1", "nl2", "us1", "us2", "jp1", "jp2", "jp3"]
-defaultVpn :: String
-defaultVpn = "nl1"
+servers :: [String]
+servers = ["nl1", "nl2", "us1", "us2", "jp1", "jp2", "jp3"]
+defaultServer :: String
+defaultServer = "nl1"
 
 nextInList :: String -> String
-nextInList c = case fmap (\i -> vpns !! if i + 1 == length vpns then 0 else i + 1) (c `elemIndex` vpns) of
-  Just country -> country
+nextInList c = case fmap (\i -> servers !! if i + 1 == length servers then 0 else i + 1) (c `elemIndex` servers) of
+  Just server -> server
   Nothing -> error "Error: something unexpected in finding next vpn in the list"
 
 
