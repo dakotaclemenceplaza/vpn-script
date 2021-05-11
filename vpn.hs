3702335660c1c2f252c33e6dd0facc8c25fa8ee2
A D <dakotaclemenceplaza@protonmail.com>
2021-05-12 02:10:15 +0400

    Add reading servers from conf files in /etc/openvpn

--- vpn.hs
+++ vpn.hs
@@ -8,40 +8,40 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import System.Environment (getArgs)
-import System.Directory (createDirectoryIfMissing, removeFile)
+import System.Directory (createDirectoryIfMissing, removeFile, listDirectory)
 import System.IO (hPutStrLn, stderr)
 import System.Process.Typed (runProcess, runProcess_, readProcess, shell)
 import Control.Exception (IOException, catch)
-import Data.List (elemIndex)
+import Data.List (elemIndex, isSuffixOf)
 import Control.Monad (void)
 import Network.HTTP.Simple (httpJSON, getResponseBody)
 import Data.Aeson (Value)
 import qualified Data.Text as T (Text)
 import DBus.Notify (summary, body, connectSession, notify, blankNote, Body(Text))
-import Control.Monad.Except (ExceptT, runExceptT, lift, throwError)
+import Control.Monad.Except (ExceptT, runExceptT, liftIO, throwError)
+import Control.Monad.Reader (ReaderT, runReaderT, ask)
 import Control.Concurrent (threadDelay)
 import Control.Lens ((^?))
 import Data.Aeson.Lens (key, _String)
 
-servers :: [String]
-servers = ["nl1", "nl2", "us1", "us2", "jp1", "jp2", "jp3"]
 defaultServer :: String
 defaultServer = "nl1"
 
 type Server = String
 data Vpn = Off | On Server
 data Result = V Vpn | Message String
-type VpnToggle a = ExceptT String IO a
+type VpnToggle a = ExceptT String (ReaderT [Server] IO) a
 
 data Notify = CLI | Desktop
 
 main :: IO ()
 main = do
   args <- getArgs
+  servers <- listDirectory "/etc/openvpn" >>= pure . map (takeWhile (/='.')) . filter (".conf" `isSuffixOf`)
   end <- case args of
-           ["desk"] -> runExceptT $ run Desktop []
-           [arg, "desk"] -> runExceptT $ run Desktop [arg]
-           other -> runExceptT $ run CLI other
+           ["desk"] -> runReaderT (runExceptT $ run Desktop []) servers
+           [arg, "desk"] -> runReaderT (runExceptT $ run Desktop [arg]) servers
+           other -> runReaderT (runExceptT $ run CLI other) servers
   case end of
     Right _ -> pure ()
     Left e -> hPutStrLn stderr e
@@ -58,22 +58,24 @@ run notifyMode arg = let outRes = outputResult notifyMode
                           ["next"] -> next >>= outRes
                           ["stop"] -> stop >>= outRes
                           ["clean"] -> clean >>= outRes
-                          [server] -> if server `elem` servers
-                                      then start server >>= outRes
-                                      else outRes $ Message "No such vpn server"
+                          [server] -> do
+                            servers <- ask
+                            if server `elem` servers
+                              then start server >>= outRes
+                              else outRes $ Message "No such vpn server"
                           _ -> outRes $ Message "Unknown arguments"
 
 outputResult :: Notify -> Result -> VpnToggle ()
 outputResult CLI (V (On server)) = do
-  lift $ threadDelay 3000000 -- how long to wait?
-  getLocation >>= lift . putStrLn . locationString server . show
+  liftIO $ threadDelay 3000000 -- how long to wait?
+  getLocation >>= liftIO . putStrLn . locationString server . show
 outputResult Desktop (V (On server)) = do
-  lift $ threadDelay 3000000
-  getLocation >>= lift . notifyDesktop . locationString server . show
-outputResult CLI (V Off) = lift $ putStrLn "Vpn is stopped"
-outputResult CLI (Message m) = lift $ putStrLn m
-outputResult Desktop (V Off) = lift $ notifyDesktop "Vpn is stopped"
-outputResult Desktop (Message m) = lift $ notifyDesktop m
+  liftIO $ threadDelay 3000000
+  getLocation >>= liftIO . notifyDesktop . locationString server . show
+outputResult CLI (V Off) = liftIO $ putStrLn "Vpn is stopped"
+outputResult CLI (Message m) = liftIO $ putStrLn m
+outputResult Desktop (V Off) = liftIO $ notifyDesktop "Vpn is stopped"
+outputResult Desktop (Message m) = liftIO $ notifyDesktop m
 
 start :: String -> VpnToggle Result
 start server = do
@@ -83,7 +85,7 @@ start server = do
     Off -> do
       stat <- status server
       case stat of
-        Off -> lift $ do
+        Off -> liftIO $ do
           -- next line returns error exit code but it works fine, so use runProcess and
           -- throw away exit code instead of runProcess_ which checks it and rethrows
           void $ runProcess $ shell $ "sudo /etc/init.d/openvpn." <> server <> " start"
@@ -103,7 +105,7 @@ stop :: VpnToggle Result
 stop = do
   current <- currentVpn
   case current of
-    On server -> lift $ do
+    On server -> liftIO $ do
       runProcess_ $ shell $ "sudo /etc/init.d/openvpn." <> server <> " stop" 
       removeFile "/tmp/vpn/current" -- add checking status of just stopped?
       pure $ V Off
@@ -111,9 +113,9 @@ stop = do
 
 clean :: VpnToggle Result
 clean = do
-  vpnInTmp <- lift $ fmap On (readFile "/tmp/vpn/current") `catch` exceptionHandler
+  vpnInTmp <- liftIO $ fmap On (readFile "/tmp/vpn/current") `catch` exceptionHandler
   case vpnInTmp of
-    On server -> lift $ do
+    On server -> liftIO $ do
       runProcess_ $ shell $ "sudo /etc/init.d/openvpn." <> server <> " stop" 
       removeFile "/tmp/vpn/current"
       pure $ V Off
@@ -123,7 +125,7 @@ clean = do
 
 currentVpn :: VpnToggle Vpn
 currentVpn = do
-  vpnInTmp <- lift $ fmap On (readFile "/tmp/vpn/current") `catch` exceptionHandler
+  vpnInTmp <- liftIO $ fmap On (readFile "/tmp/vpn/current") `catch` exceptionHandler
   case vpnInTmp of
     On server -> do
       vpnStatus <- status server
@@ -136,7 +138,7 @@ currentVpn = do
 
 status :: String -> VpnToggle Vpn
 status server = do
-  (_, out, _) <- lift $ readProcess $ shell $ "sudo /etc/init.d/openvpn." <> server <> " status"
+  (_, out, _) <- liftIO $ readProcess $ shell $ "sudo /etc/init.d/openvpn." <> server <> " status"
   case out of
     " * status: stopped\n" -> pure Off
     " * status: started\n" -> pure $ On server
@@ -144,7 +146,7 @@ status server = do
 
 getLocation :: VpnToggle T.Text
 getLocation = do
-  response <- lift $ getResponseBody <$> httpJSON "http://ifconfig.co/json" :: VpnToggle Value
+  response <- liftIO $ getResponseBody <$> httpJSON "http://ifconfig.co/json" :: VpnToggle Value
   case response ^? key "country" . _String of
     Just c -> pure c
     _ -> throwError "Error: something unexpected in the response from getting location"
@@ -156,9 +158,11 @@ notifyDesktop message = do
                                     body = Just $ Text message}
 
 nextInList :: String -> VpnToggle String
-nextInList c = case fmap (\i -> servers !! if i + 1 == length servers then 0 else i + 1) (c `elemIndex` servers) of
-  Just server -> pure server
-  Nothing -> throwError "Error: something unexpected in finding next vpn in the list"
+nextInList c = do
+  servers <- ask
+  case fmap (\i -> servers !! if i + 1 == length servers then 0 else i + 1) (c `elemIndex` servers) of
+    Just server -> pure server
+    Nothing -> throwError "Error: something unexpected in finding next vpn in the list"
 
 locationString :: String -> String -> String
 locationString server location = "Vpn is started. Server: " <> server <> ". Location: " <> init (tail location) <> "."
